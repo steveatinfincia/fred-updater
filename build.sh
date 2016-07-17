3b92b2f436b6ae3f7ad1a92058ee98def161a323
Stephen Oliver <steve@infincia.com>
2016-07-17 15:36:58 -0400

    Force use of Python 2 for pyinstaller. tested on macOS & Gentoo
    Should also work on Ubuntu/Debian

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@
 
 
 
-PLATFORM=`python -c "import platform; print platform.system().lower()"`
-ARCH=`python -c "import sys; arch = lambda(is_64bit): "64" if is_64bit else "32"; print arch(sys.maxsize > 2**32)"`
+PLATFORM=`python2.7 -c "import platform; print platform.system().lower()"`
+ARCH=`python2.7 -c "import sys; arch = lambda(is_64bit): "64" if is_64bit else "32"; print arch(sys.maxsize > 2**32)"`
 
 echo "Building for $PLATFORM-$ARCH"
 
@@ -19,7 +19,7 @@ rm -rf installer_env client_env build dist update-$PLATFORM-$ARCH.spec
 
 
 # set up build environment
-virtualenv -p python installer_env
+virtualenv -p python2.7 installer_env
 source installer_env/bin/activate
 pip install pyinstaller
 deactivate
@@ -27,7 +27,7 @@ deactivate
 
 
 # set up client environment and build 
-virtualenv -p python client_env
+virtualenv -p python2.7 client_env
 source client_env/bin/activate
 pip install tuf
 installer_env/bin/pyinstaller --onefile client/update.py --name "update-$PLATFORM-$ARCH"
